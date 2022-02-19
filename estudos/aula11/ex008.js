c9bf56061d2f54ca144cb63d27fb3b42b61d8161
LucasGBurch <88804181+LucasGBurch@users.noreply.github.com>
2022-02-19 14:53:20 -0300

    Condicionais #1
    Uso de if e if/else em documentos JS usando extensão NodeExec. Aplicação desses exemplos em tag de script em dois sites inteiros (html/css/js) para praticar os condicionais junto com funções embutidas em botão de input.

---
+++ estudos/aula11/ex008.js
@@ -0,0 +1,10 @@
+console.log('Teste de Console do NodeExec para VSCode.') // extensão Roda em F8
+
+var vel = 60.0
+console.log(`A velocidade do seu carro é de ${vel}km/h.`)
+
+if (vel > 60) { // praticando a sintaxe com base na aula para não perder o costume.
+    console.log(`Você ultrapassou o limite de velocidade.`)
+} else {
+    console.log(`Você está dentro do limite de velocidade.`)
+}
