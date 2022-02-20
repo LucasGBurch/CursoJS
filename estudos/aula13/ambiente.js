0e73015d7cbcf27f68644309c651221f75a13a7f
LucasGBurch <88804181+LucasGBurch@users.noreply.github.com>
2022-02-20 18:56:16 -0300

    Estruturas de Repetição
    Revendo programação bem básica. While, do while e for. A serem aplicados nos exercícios subsequentes. Prof aproveitou pra mostrar a depuração (debug) do VSCode ao mostrar o for( ; ; )

---
+++ estudos/aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+// Laços de repetição tipo while (teste lógico no início)
+while(c <= 6) {
+    console.log(`Mensagem ${c}`)
+    c++
+}
+
+// Tipo do {} while (teste lógico no fim)
+c = 0 // reiniciando em 0
+do {
+    console.log(`Mensagem ${c}`)
+    c++
+} while(c <= 5)
