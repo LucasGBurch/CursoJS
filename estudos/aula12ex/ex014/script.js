042562a733044c99834f9a05d668592ac539785a
LucasGBurch <88804181+LucasGBurch@users.noreply.github.com>
2022-02-20 10:35:31 -0300

    Fonte e edição de imagens
    Só para registrar fonte do Pexels e edição com Gimp.

--- estudos/aula12ex/ex014/script.js
+++ estudos/aula12ex/ex014/script.js
@@ -6,8 +6,8 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 6 && hora < 12) {
-        img.src = 'imgmanha.png'
-        document.body.style.background = '#82bbe4'
+        img.src = 'imgmanha.png' // imagens pesquisadas no Pexels e editadas no Gimp
+        document.body.style.background = '#82bbe4' // (prof usou photoshop)
     } else if (hora >= 12 && hora <= 18) {
         img.src = 'imgtarde.png'
         document.body.style.background = '#e69279'
